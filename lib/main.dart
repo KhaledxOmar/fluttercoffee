27e2e7499f831ecaf2021b41f70944f0d5cf70d6
KhaledxOmar <mr.joker2017@gmail.com>
2024-04-22 15:35:58 +0300

    create main.lib

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/Home.dart';
+import 'package:first_app/gradient_container.dart';
+void main() {
+  
+  runApp(
+    const MaterialApp(
+      home: Home()
+      )
+    );
+  
+}
+
+
