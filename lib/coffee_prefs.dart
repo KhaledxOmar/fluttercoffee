cf0a2f262847bb515e371d3e007f80fd06cd4768
KhaledxOmar <mr.joker2017@gmail.com>
2024-04-27 07:02:03 +0300

    assets file added, pictures is added in the app and buttons for sugar and strength is added

--- lib/coffee_prefs.dart
+++ lib/coffee_prefs.dart
@@ -3,21 +3,55 @@ import 'package:flutter/material.dart';
 class CoffeePrefs extends StatelessWidget{
     const CoffeePrefs({super.key});
 
+    void increasesugars(){
+      print("sugar added");
+    }
+
       @override
       Widget build(context) {
-        return const Column(
+        return Column(
           children: [
             Row(children: [
-              Text("strength: "),
-              Text("4"),
-              SizedBox(width: 50),
+              const Text("strength: "),
+              const Text("5"),
+              Image.asset('assets/img/coffee_bean.png',
+                width: 25,
+                color: Colors.brown[200],
+                colorBlendMode: BlendMode.multiply,
+                ),
+              const Expanded(child: SizedBox(width: 50)),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.brown,
+                  foregroundColor: Colors.white,
+                ),
+                onPressed: increasesugars, 
+                child: const Text("+"),
+                ),
+
+              
+
             ]
             ),
 
             Row(children: [
-              Text("strength: "),
-              Text("34"),
-              SizedBox(width: 50),
+              const Text("strength: "),
+              const Text("34"),
+              Image.asset('assets/img/sugar_cube.png',
+                width: 25,
+                color: Colors.brown[200],
+                colorBlendMode: BlendMode.multiply,
+                ),
+              const Expanded(child: SizedBox(width: 50)),
+              
+              FilledButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.brown,
+                  foregroundColor: Colors.white,
+                ),
+                onPressed: increasesugars, 
+                child: const Text("+"),
+                ),
             ]
             ),
           ]
